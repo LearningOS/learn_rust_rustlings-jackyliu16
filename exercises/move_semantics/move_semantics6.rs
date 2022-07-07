34df46eca26a03e34ff965dc251352da1b7a5419
jackyLiu <18922251299@163.com>
2022-07-07 20:12:58 +0800

    update

--- exercises/move_semantics/move_semantics6.rs
+++ exercises/move_semantics/move_semantics6.rs
@@ -2,24 +2,24 @@
 // Make me compile! `rustlings hint move_semantics6` for hints
 // You can't change anything except adding or removing references
 
-// I AM NOT DONE
 
 fn main() {
-    let data = "Rust is great!".to_string();
+    let mut data = "Rust is great!".to_string();    // String 
 
-    get_char(data);
+    get_char(&data);                                // just give a kind of borrow to them 
 
-    string_uppercase(&data);
+    string_uppercase(&mut data);                    // giving a kind of mutiable reference
 }
 
 // Should not take ownership
-fn get_char(data: String) -> char {
+fn get_char(data: &String) -> char {                // a kind of borrow
     data.chars().last().unwrap()
 }
 
 // Should take ownership
-fn string_uppercase(mut data: &String) {
-    data = &data.to_uppercase();
+fn string_uppercase(data: &mut String) {            // mutiable reference 
+    
+    let data = &data.to_uppercase();
 
     println!("{}", data);
 }
